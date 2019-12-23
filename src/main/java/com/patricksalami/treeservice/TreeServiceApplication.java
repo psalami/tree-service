54bf6c5fde6129c46a70fa0102d626d63e637d80
Patrick Salami <patrick@patricksalami.com>
2019-12-23 07:56:17 -0800

    setting up application

---
+++ src/main/java/com/patricksalami/treeservice/TreeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.patricksalami.treeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeServiceApplication.class, args);
+	}
+
+}
